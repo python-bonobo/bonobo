89dee0e65aa2a411b0a9c6921804190abcc4d32a
Romain Dorgueil <romain@dorgueil.net>
2016-12-26 10:47:30 +0100

    wip: documentation

--- doc/conf.py
+++ doc/conf.py
@@ -90,6 +90,11 @@ html_sidebars = {
     ]
 }
 
+
+html_theme_path = ['_themes']
+html_additional_pages = {'index': 'index.html'}
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
