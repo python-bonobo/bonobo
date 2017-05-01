b4716f9ceee0cfa097895e8b051a12dd2d453127
Romain Dorgueil <romain@dorgueil.net>
2017-05-01 08:14:09 +0200

    fix json example (#37)

---
+++ bonobo/examples/files/json_handlers.py
@@ -0,0 +1,14 @@
+import bonobo
+from bonobo.commands.run import get_default_services
+
+def get_fields(row):
+    return row['fields']
+
+graph = bonobo.Graph(
+    bonobo.JsonReader(path='datasets/theaters.json'),
+    get_fields,
+    bonobo.PrettyPrint(title_keys=('eq_nom_equipement', )),
+)
+
+if __name__ == '__main__':
+    bonobo.run(graph, services=get_default_services(__file__))
