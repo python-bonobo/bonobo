9370f6504e0c3228afabda76947c2ef015bbb5a1
Romain Dorgueil <romain@dorgueil.net>
2017-05-28 16:51:02 +0200

    [ext] Adds docker extra to setup.py

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
             'coverage (>= 4.4, < 5.0)', 'pytest (>= 3.1, < 4.0)', 'pytest-cov (>= 2.5, < 3.0)',
             'pytest-timeout (>= 1, < 2)', 'sphinx (>= 1.6, < 2.0)'
         ],
+        'docker': ['bonobo-docker'],
         'jupyter': ['ipywidgets (>= 6.0.0.beta5)', 'jupyter (>= 1.0, < 1.1)']
     },
     entry_points={
