bad598a4d7ce3986f61b4db4d0f6e0572bf61ac6
cwandrews <cwandrews.oh@gmail.com>
2017-10-15 16:52:14 -0400

    Cast env_dir to string before passing to load_dotenv as passing a PosixPath to load_dotenv raises an exception in 3.5.

--- bonobo/commands/run.py
+++ bonobo/commands/run.py
@@ -80,14 +80,14 @@ def read(filename, module, install=False, quiet=False, verbose=False, default_en
     env_dir = Path(filename).parent or Path(module).parent
     if default_env_file:
         for f in default_env_file:
-            env_file_path = env_dir.joinpath(f)
+            env_file_path = str(env_dir.joinpath(f))
             load_dotenv(env_file_path)
     if default_env:
         for e in default_env:
             set_env_var(e)
     if env_file:
         for f in env_file:
-            env_file_path = env_dir.joinpath(f)
+            env_file_path = str(env_dir.joinpath(f))
             load_dotenv(env_file_path, override=True)
     if env:
         for e in env:
