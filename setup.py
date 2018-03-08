cd04354d3da6bf6278ea2c11a55891899717136b
Romain Dorgueil <romain@dorgueil.net>
2018-03-08 11:26:27 +0100

    Update requirements.

--- setup.py
+++ setup.py
@@ -64,9 +64,9 @@ setup(
     ],
     extras_require={
         'dev': [
-            'cookiecutter (>= 1.5, < 1.6)', 'coverage (>= 4.4, < 5.0)', 'pytest (>= 3.1, < 4.0)',
-            'pytest-cov (>= 2.5, < 3.0)', 'pytest-sugar (>= 0.8, < 0.9)', 'pytest-timeout (>= 1, < 2)',
-            'sphinx (>= 1.6, < 2.0)', 'sphinx-sitemap (>= 0.2, < 0.3)', 'yapf'
+            'cookiecutter (>= 1.5, < 1.6)', 'coverage (~= 4.4)', 'pytest (~= 3.4)', 'pytest-cov (~= 2.5)',
+            'pytest-sugar (~= 0.9.1)', 'pytest-timeout (>= 1, < 2)', 'sphinx (>= 1.6, < 2.0)',
+            'sphinx-sitemap (>= 0.2, < 0.3)', 'yapf'
         ],
         'docker': ['bonobo-docker (~= 0.6.0a1)'],
         'jupyter': ['ipywidgets (~= 6.0)', 'jupyter (~= 1.0)'],
