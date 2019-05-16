c6818dc960860a118409c0465f2825999888233f
Romain Dorgueil <romain@dorgueil.net>
2019-05-16 10:03:30 +0100

    Update requirements, fix formating.

--- bonobo/contrib/django/__init__.py
+++ bonobo/contrib/django/__init__.py
@@ -6,7 +6,7 @@ This module contains all tools for Bonobo and Django to interract nicely.
 
 """
 
-from .utils import create_or_update
 from .commands import ETLCommand
+from .utils import create_or_update
 
 __all__ = ["ETLCommand", "create_or_update"]
