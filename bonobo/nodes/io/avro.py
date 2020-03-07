27809bbcd8dcd8a521b5b72d63a7471c18bf552e
Juarez Rudsatz <juarezr@gmail.com>
2020-03-07 01:23:05 -0300

    avro: add prop schema as dict do AvroWriter

--- bonobo/nodes/io/avro.py
+++ bonobo/nodes/io/avro.py
@@ -4,89 +4,88 @@ from decimal import Decimal
 from bonobo.config import Option, use_context
 from bonobo.nodes.io.base import FileHandler
 from bonobo.nodes.io.file import FileReader, FileWriter
-from bonobo.util.collections import coalesce
 
-import fastavro
+# import fastavro
+try:
+    import fastavro
+except ImportError:
+    pass
 
 
 class AvroHandler(FileHandler):
-    """
+    schema = Option(tuple, required=False, 
+        __doc__="A dict specifying the schema fields acording avro spec.\
+        ```\
+        schema = {\
+            'doc': 'A weather reading.',\
+            'name': 'Weather',\
+            'namespace': 'test',\
+            'type': 'record',\
+            'fields': [\
+                {'name': 'station', 'type': 'string'},\
+                {'name': 'date', 'type': 'int', 'logicalType': 'date'},\
+                {'name': 'time', 'type': 'long', 'logicalType': 'time-micros'},\
+                {'name': 'temp', 'type': 'int'},\
+            ],\
+        }\
+        ```\
+        See: https://avro.apache.org/docs/current/spec.html#schema_primitive\
+             https://avro.apache.org/docs/current/spec.html#Logical+Types")
 
-    .. attribute:: item_names
-
-        The names of the items in the Avro, if it is not defined in the first item of the Avro.
-
-    """
-
-    fields = Option(tuple, required=False)
-    types = Option(tuple, required=False)
-    name = Option(str, required=False)
-    namespace = Option(str, required=False)
-    doc = Option(str, required=False)
-    codec = Option(str, required=False, default="null")
+    codec = Option(str, required=False, default="null", 
+        __doc__="The name of the compression codec used to compress blocks.\
+                Compression codec can be ‘null’, ‘deflate’ or ‘snappy’ (if installed)")
 
 
 @use_context
 class AvroReader(FileReader, AvroHandler):
     """
-    Reads a record from avro file and yields the rows in dicts.
+    Reads the records from a avro file and yields the values in dicts.
     """
-
     mode = Option(str, default="rb")
 
     def load_schema(self, context, avro_reader):
-        aschema = avro_reader.schema
-        if 'doc' in aschema:
-            self.doc = aschema['doc']
-        if 'name' in aschema:
-            self.name = aschema['name']
-        if 'namespace' in aschema:
-            self.namespace = aschema['namespace']
-        src = aschema['fields']
-        dst = [f['name'] for f in src]
-        tfields = tuple(dst)
-        context.set_output_fields(tfields)
+        file_schema = avro_reader.writer_schema
+        self.schema = file_schema
+        self.codec = avro_reader.codec
+        schema_fields = file_schema['fields']
+        field_names = [col['name'] for col in schema_fields]
+        col_names = tuple(field_names)
+        context.set_output_fields(col_names)
 
     def read(self, file, context, *, fs):
         avro_reader = fastavro.reader(file)
         if not context.output_type:
             self.load_schema(context, avro_reader)
-
-        for row in avro_reader:
-            yield tuple(row)
+        for record in avro_reader:
+            row = tuple(record.values())
+            yield row
 
     __call__ = read
 
 
 @use_context
 class AvroWriter(FileWriter, AvroHandler):
+    """
+    Writes the values as records into a avro file according to the fields defined in schema
+
+    When the schema is not specified, it tries to guess types from the values
+    of the fields present in the first record. 
+    The type of values written follow the ones of python type system. Take
+    care when writing data extracted from sql databases as their types are
+    usually affected by factors like driver issues, type mismatch, incorrect
+    mapping, precision loss (specially float and decimals), SQLArchitect...
+    """
+    compression_level = Option(int, required=False, 
+        __doc__="Compression level to use when the specified codec supports it")
 
     mode = Option(str, default="wb+")
 
     def assure_same_len(self, props, values):
         if len(props) != len(values):
-            raise ValueError(
-                "Values length differs from input fields length. Expected: {}. Got: {}. Values: {!r}.".format(
-                    len(props), len(values), values
-                )
-            )
-
-    def build_schema_from_types(self, props):
-        schema_fields = []
-        for p, t in zip(props, self.types):
-            if  isinstance(t, dict):
-                f = t
-                f['name'] = p
-            elif t == "date":
-                f = {'name': p, 'type': 'int', "logicalType": "date"}
-            elif t == "time-micros":
-                f = {'name': p, 'type': 'long', "logicalType": "time-micros"}
-            elif t == "timestamp-micros":
-                f = {'name': p, 'type': 'long', "logicalType": "timestamp-micros"}
-            else:
-                f = {'name': p, 'type': t}
-            schema_fields.append(f)
-        return schema_fields
+            m = "Values length differs from input fields length. Expected: {}. Got: {}. Values: {!r}."
+            f = m.format(len(props), len(values), values)
+            raise ValueError(m)
 
     def build_schema_from_values(self, props, values):
         # https://avro.apache.org/docs/current/spec.html#schema_primitive
@@ -100,11 +99,11 @@ class AvroWriter(FileWriter, AvroHandler):
             elif isinstance(v, float):
                 f = {'name': p, 'type': 'double'}
             elif isinstance(v, date):
-                f = {'name': p, 'type': 'int', "logicalType": "date"}
+                f = {'name': p, 'type': 'int', 'logicalType': 'date'}
             elif isinstance(v, timedelta) or isinstance(v, time):
-                f = {'name': p, 'type': 'long', "logicalType": "time-micros"}
+                f = {'name': p, 'type': 'long', 'logicalType': 'time-micros'}
             elif isinstance(v, datetime):
-                f = {'name': p, 'type': 'long', "logicalType": "timestamp-micros"}
+                f = {'name': p, 'type': 'long', 'logicalType': 'timestamp-micros'}
             elif isinstance(v, Decimal):
                 f = {'name': p, 'type': 'double'}
             elif isinstance(v, bytes):
@@ -160,45 +159,41 @@ class AvroWriter(FileWriter, AvroHandler):
     def get_value_as_float(value):
         return float(value)
 
-    def build_schema(self, props, values):
-
-        if self.types is not None:
-            schema_fields = self.build_schema_from_types(props)
-        else:
-            schema_fields = self.build_schema_from_values(props, values)
-
+    def build_schema_from(self, props, values):
+        if self.schema is not None:
+            return self.schema
+        schema_fields = self.build_schema_from_values(props, values)
         schema = {
             'type': 'record',
-            'name': coalesce(self.name, "output"),
-            'namespace':  coalesce(self.namespace, "avro"),
-            'doc': coalesce(self.doc, "generated by bonobo"),
+            'name': 'output',
+            'namespace': "avro",
+            'doc': "generated by bonobo",
             'fields': schema_fields,
         }
         return schema
 
-    def get_write_fields(self, context):
-        props = coalesce(self.fields, context.get_input_fields())
-        return props
-
     def write(self, file, context, *values, fs):
         """
-        Write a record to the opened file.
+        Write a record to the opened file using the defined schema
         """
-        props = self.get_write_fields(context)
-
         context.setdefault("schema", None)
-        if not context.schema:
-            aschema = self.build_schema(props, values)
-            parsed_schema = fastavro.parse_schema(aschema)
-            context.schema = parsed_schema
-
         context.setdefault("converters", None)
+
+        props = context.get_input_fields()
+        if not context.schema:
+            detected = self.build_schema_from(props, values)
+            parsed = fastavro.parse_schema(detected)
+            context.schema = parsed
         if not context.converters:
             context.converters = self.build_converters_from_values(values)
-
-        kv = {k: conv(v) for k, v, conv in zip(props, values, context.converters)}
-
-        row = [kv]
-        fastavro.writer(fo=file, schema=context.schema, records=row, codec=self.codec)
+        row = {k: conv(v) for k, v, conv in zip(props, values, context.converters)}
+        one_record = [row]
+        fastavro.writer(
+            fo=file, 
+            schema=context.schema, 
+            records=one_record, 
+            codec=self.codec,
+            codec_compression_level=self.compression_level
+            )
 
     __call__ = write
